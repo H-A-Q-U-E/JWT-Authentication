32fe80e342c89eab15d87d7648cbf1c9eb6401e3
haque <rhaque970@gmail.com>
2024-09-30 18:37:16 +0530

    Initial commit

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,24 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
 
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+const secret = "hAque@123@";
+
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports = {
